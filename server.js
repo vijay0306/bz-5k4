d635b010aad975b5a7b479365287ba8089267e52
vijay0306 <vijaykumar.ellendula@gmail.com>
2020-01-08 10:52:17 +0530

    Changes for angular app with routing

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express()
 const port =process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/dist/ngproject'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/ngproject/index.html'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/ngproject/index.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
